c90ed7151bf2cc122bbee905b21f9a095602cac7
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-11-06 10:36:10 +0100

    added xamarin forms project

---
+++ Feedbacker/Feedbacker/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Feedbacker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=YOUR_APP_SECRET_HERE;",
+                  typeof(Analytics), typeof(Crashes));            
+        }
+
+        protected override void OnSleep()
+        {            
+        }
+
+        protected override void OnResume()
+        {            
+        }
+    }
+}
